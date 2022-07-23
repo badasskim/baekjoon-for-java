88542301aa1fb9e6cfa6a26a25fbe33c729287c9
badasskim <rladhkdwls520@naver.com>
2022-07-24 03:52:14 +0900

    헝헝헝

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,10 @@
-import java.io.*;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
 
-//11047 동전 0
 public class Main
 {
-    public static void main(String[] args) throws IOException
+    public static void main(String[] args)
     {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        ArrayList<Integer> arr = new ArrayList<>();
-        int count = 0;
-
-        while(N-- > 0)
-        {
-            arr.add(Integer.parseInt(br.readLine()));
-        }
-
-        for(int i = arr.size()-1; i >=0; i--)
-        {
-            if(K == 0) break;
-            if(arr.get(i) <= K)
-            {
-                count += K / arr.get(i);
-                K = K%arr.get(i);
-            }
-        }
-
-        System.out.println(count);
-
+        System.out.println("하하하!");
     }
+
 }
 
